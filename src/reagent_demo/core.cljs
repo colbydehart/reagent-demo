02d9f6dc3c3884b37ac13c9fecb74f89a1ac07b3
Colby DeHart <colbydehart@gmail.com>
2016-08-09 23:53:18 -0400

    add-data

--- src/reagent_demo/core.cljs
+++ src/reagent_demo/core.cljs
@@ -1,17 +1,33 @@
 (ns reagent-demo.core
-    (:require [reagent.core :as reagent :refer [atom]]))
+    (:require [reagent.core :as reagent]))
+
+;; -------------------------
+;; Data
+(def timers [1 4 8])
 
 ;; -------------------------
 ;; Views
+(defn timer-component [timer]
+  [:li "Time: " timer])
 
-(defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+(defn home-page
+  "Home page of application"
+  []
+  [:div
+   [:h2 "Multi-Timer"]
+
+   [:ul.timers
+    (for [timer timers]
+      [timer-component timer])]
+   [:button "Add Timer"]])
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (reagent/render [home-page] (.getElementById js/document "app")))
+  (reagent/render
+   [home-page]
+   (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
